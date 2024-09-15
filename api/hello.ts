5b1cf9a41d02bdf57278e2c407bb5b4d45edf50d
BenocxX <mathis.cote@outlook.com>
2024-09-14 23:48:56 -0400

    feat: add prisma database

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,7 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
 export function GET(_: Request) {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
