650cffe1396c254cc336a5cbf6a246f23b68a991
Alexandre-Collette <90921489+Alexandre-Collette@users.noreply.github.com>
2024-09-15 14:42:49 -0400

    feat : added based file for api

--- api/reservation.js
+++ api/reservation.js
@@ -6,9 +6,9 @@ export async function GET(request) {
   try {
     const user = await prisma.user.findFirst();
     if (!user) {
-      return new Response('User not found', { status: 404 });
+      return new Response('Not found', { status: 404 });
     }
-    return new Response(`Hello from ${user.email}.`);
+    return new Response(`Good`);
   } catch (error) {
     return new Response('Something went wrong', { status: 500 });
   }
