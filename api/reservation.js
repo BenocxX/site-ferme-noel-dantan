96ede79ea6d174e82137bb27771cb3d35c050b50
Alexandre-Collette <90921489+Alexandre-Collette@users.noreply.github.com>
2024-09-15 14:53:08 -0400

    chore : changed date field in OpenDay to Datetime

--- api/reservation.js
+++ api/reservation.js
@@ -3,13 +3,5 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET(request) {
-  try {
-    const user = await prisma.user.findFirst();
-    if (!user) {
-      return new Response('Not found', { status: 404 });
-    }
-    return new Response(`Good`);
-  } catch (error) {
-    return new Response('Something went wrong', { status: 500 });
-  }
+  return new Response(`${request}`);
 }
