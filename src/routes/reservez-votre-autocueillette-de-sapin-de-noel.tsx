7a954a282b50c8283bc0b65d09efb547286413a1
BenocxX <mathis.cote@outlook.com>
2024-10-10 14:22:17 -0400

    fix: redirect before load

--- src/routes/reservez-votre-autocueillette-de-sapin-de-noel.tsx
+++ src/routes/reservez-votre-autocueillette-de-sapin-de-noel.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/reservez-votre-autocueillette-de-sapin-de-noel')({
+  beforeLoad: () => {
+    throw redirect({
+      to: '/',
+    });
+  },
   loader: () => {
     throw redirect({
       to: '/',
